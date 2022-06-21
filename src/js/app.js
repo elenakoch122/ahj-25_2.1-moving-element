615878a051dbd56cff44b505398d4a252774af0f
Elena Kochegarova <lelet@ngs.ru>
2022-06-21 14:44:35 +0700

    Initial commit

---
+++ src/js/app.js
@@ -0,0 +1,24 @@
+const field = document.getElementById('field');
+const fieldSize = 4;
+const fieldArr = [];
+const img = document.createElement('img');
+img.classList.add('img');
+img.src = 'img/goblin.png';
+// img.src = '../img/goblin.png';
+// img.src = 'https://github.com/netology-code/ahj-homeworks/raw/simplification/dom/pic/goblin.png';
+img.alt = 'Красное лицо гоблина';
+
+for (let i = 0; i < fieldSize * fieldSize; i += 1) {
+  const cell = document.createElement('div');
+  cell.classList.add('cell');
+  field.appendChild(cell);
+  fieldArr.push(cell);
+}
+
+setInterval(() => {
+  const currentCell = fieldArr.find((item) => item.querySelector('img'));
+  if (currentCell) {
+    currentCell.querySelector('img').remove();
+  }
+  fieldArr[Math.floor(Math.random() * fieldArr.length)].append(img);
+}, 1000);
